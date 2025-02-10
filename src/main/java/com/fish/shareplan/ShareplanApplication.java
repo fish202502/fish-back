35014faff2fa2ab495f9cdfb4c12485fdddc5ce5
samchi <seyoung0314@gmail.com>
2025-02-10 17:27:37 +0900

    initial commit

---
+++ src/main/java/com/fish/shareplan/ShareplanApplication.java
@@ -0,0 +1,13 @@
+package com.fish.shareplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareplanApplication.class, args);
+	}
+
+}
